2cb56e7b1e9314ec9a8034fd69a372b6dd685b84
Drake Nguyen <drakehanguyen@gmail.com>
2025-09-22 20:29:34 -0500

    Update (#5)
    Co-authored-by: DrakeNguyen <drake.ha.nguyen@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/devpockit',
   output: 'export', // Enable static export for GitHub Pages
   trailingSlash: true, // Required for GitHub Pages
   images: {
